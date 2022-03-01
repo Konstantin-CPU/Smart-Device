745c2e16e767590551b7d7997c4886625f6e4f54
Константин <konstantingoncharuk50@gmail.com>
2022-03-01 11:58:10 +0300

    Небольшие изменения перед тестированием

--- source/js/main.js
+++ source/js/main.js
@@ -35,32 +35,40 @@ window.addEventListener("load", function() {
     pageHeaderToggle.addEventListener("click", function() {
       popupForm.classList.remove("visually-hidden");
       document.querySelector(".page__body-overlay").classList.remove("visually-hidden");
+      document.querySelector(".page__body").classList.add("page__body--scroll-disabled");
       document.querySelector(".popup-form__name").focus();
     })
 
     popupFormToggle.addEventListener("click", function() {
       document.querySelector(".popup-form").classList.add("visually-hidden");
       document.querySelector(".page__body-overlay").classList.add("visually-hidden");
+      document.querySelector(".page__body").classList.remove("page__body--scroll-disabled");
     })
 
     document.addEventListener('keydown', (evt) => {
       if (evt.key === 'Esc' || evt.key === 'Escape') {
         popupForm.classList.add("visually-hidden");
         document.querySelector(".page__body-overlay").classList.add("visually-hidden");
+        document.querySelector(".page__body").classList.remove("page__body--scroll-disabled");
       }
     })
 
     document.querySelector(".page__body-overlay").addEventListener("click", function() {
       document.querySelector(".page__body-overlay").classList.add("visually-hidden");
+      document.querySelector(".page__body").classList.remove("page__body--scroll-disabled");
       popupForm.classList.add("visually-hidden");
     })
 
-    popupForm.addEventListener("submit", function() {
-      saveData();
+    popupForm.addEventListener("submit", function(e) {
+      if (document.getElementById("popup-tel").value.length < 16) {
+        e.preventDefault();
+      } else {
+        saveDataPopup();
+      }
     })
   }
 
-  function saveData() {
+  function saveDataPopup() {
     let popupName = document.getElementById("popup-name").value;
     let popupTel = document.getElementById("popup-tel").value;
     let popupQuestion = document.getElementById("popup-question").value;
@@ -70,9 +78,30 @@ window.addEventListener("load", function() {
     localStorage.setItem("userQuestion", popupQuestion);
   }
 
+  const questionsForm = document.querySelector(".questions");
+
+  if (questionsForm) {
+    questionsForm.addEventListener("submit", function(e) {
+      if (document.getElementById("questions-tel").value.length < 16) {
+        e.preventDefault();
+      } else {
+        saveDataQuestions();
+      }
+    })
+  }
+
+  function saveDataQuestions() {
+    let questionsName = document.getElementById("questions-name").value;
+    let questionsTel = document.getElementById("questions-tel").value;
+    let questionsQuestion = document.getElementById("questions-question").value;
+
+    localStorage.setItem("userName", questionsName);
+    localStorage.setItem("userTel", questionsTel);
+    localStorage.setItem("userQuestion", questionsQuestion);
+  }
+
   var maskOptions = {
-    mask: "+7(000)000-00-00",
-    lazy: false
+    mask: "+7(000)000-00-00"
   }
 
   if (document.querySelector(".telephone-input--popup")) {
